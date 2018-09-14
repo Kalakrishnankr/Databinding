aefb58a278d5e0f1733c57db07526bbd6a28d99a
Abraham Mathew <abraham.mathew@seqato.com>
2018-09-14 10:52:24 +0530

    Added data binding and fixed scroll issue

--- app/src/main/java/com/beachpartnerllc/beachpartner/user/userprofile/AthleteProfileFragment.kt
+++ app/src/main/java/com/beachpartnerllc/beachpartner/user/userprofile/AthleteProfileFragment.kt
@@ -5,45 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import com.beachpartnerllc.beachpartner.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.beachpartnerllc.beachpartner.databinding.AthleteProfileBinding
+import com.beachpartnerllc.beachpartner.etc.common.bind
 
 class AthleteProfileFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private lateinit var pager: ViewPager
-	private lateinit var tabs: SlidingTab
-	private var slideAdapter: SliderAdapter? = null
-	private val titles = arrayOf<CharSequence>("Basic Information", "More Information")
+	private lateinit var binding: AthleteProfileBinding
 	
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-
-	}
-	
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-	                          savedInstanceState: Bundle?): View? {
-		// Inflate the layout for this fragment
-		val view:View = inflater.inflate(R.layout.fragment_athlete_profile, container, false)
-		initActivity(view)
-		return view
-	}
-	
-	fun initActivity(view : View){
-		tabs = view.findViewById(R.id.tabs)
-		pager = view.findViewById(R.id.pager)
-		slideAdapter = SliderAdapter(childFragmentManager,2,titles)
-		pager.adapter = slideAdapter
-		tabs.setViewPager(pager)
-		tabs.setDistributeEvenly(true)
-		slideAdapter!!.notifyDataSetChanged()
-		tabs.setViewPager(pager)
-		
-		
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		binding = container!!.bind(R.layout.fragment_athlete_profile)
+		binding.tabs.setupWithViewPager(binding.pager)
+		binding.adapter = SliderAdapter(childFragmentManager, 2, resources.getTextArray(R.array.profile_titles))
+		return binding.root
 	}
 }
--- app/src/main/java/com/beachpartnerllc/beachpartner/user/userprofile/SliderAdapter.kt
+++ app/src/main/java/com/beachpartnerllc/beachpartner/user/userprofile/SliderAdapter.kt
@@ -1,14 +1,18 @@
 package com.beachpartnerllc.beachpartner.user.userprofile
 
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+
 
 /**
  * @author Samuel Robert <samuel.robert@seqato.com>
  * @created on 12 Sep 2018 at 12:28 PM
  */
 class SliderAdapter(fm: FragmentManager?, val numberOfTabs:Int, val titles: Array<CharSequence>) : FragmentPagerAdapter(fm) {
+	private var mCurrentPosition = -1
+	
 	override fun getItem(position: Int): Fragment? {
 		if(position==0){
 			val basicInfoFragment = BasicInfoFragment()
@@ -21,6 +25,18 @@ class SliderAdapter(fm: FragmentManager?, val numberOfTabs:Int, val titles: Arra
 			return moreInfoFragment
 		}
 
+	}
+	
+	override fun setPrimaryItem(container: ViewGroup, position: Int, `object`: Any) {
+		super.setPrimaryItem(container, position, `object`)
+		if (position != mCurrentPosition) {
+			val fragment = `object` as Fragment
+			val pager = container as CustomViewPager
+			if (fragment != null && fragment.view != null) {
+				mCurrentPosition = position
+				pager.measureCurrentView(fragment.view!!)
+			}
+		}
 	}
 	
 	override fun getPageTitle(position: Int): CharSequence {
