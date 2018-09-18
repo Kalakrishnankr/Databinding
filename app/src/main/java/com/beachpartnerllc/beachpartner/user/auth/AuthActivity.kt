e79bf519f988e236780ec7f4d2b91b2539801d31
Samuel Robert <samuel.robert@seqato.com>
2018-09-18 09:25:21 +0530

    auth: Added generated build timestamp
    Signed-off-by: Samuel Robert <samuel.robert@seqato.com>

--- app/src/main/java/com/beachpartnerllc/beachpartner/user/auth/AuthActivity.kt
+++ app/src/main/java/com/beachpartnerllc/beachpartner/user/auth/AuthActivity.kt
@@ -1,16 +1,17 @@
 package com.beachpartnerllc.beachpartner.user.auth
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.*
 import com.beachpartnerllc.beachpartner.R
+import com.beachpartnerllc.beachpartner.databinding.MainBinding
 import com.beachpartnerllc.beachpartner.etc.base.BaseActivity
 import com.beachpartnerllc.beachpartner.etc.common.getViewModel
 import com.beachpartnerllc.beachpartner.user.auth.AuthState.*
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_auth.*
 import javax.inject.Inject
 
 class AuthActivity : BaseActivity() {
@@ -19,14 +20,14 @@ class AuthActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        val binding: MainBinding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
 
         navController = findNavController(this, R.id.authNHF)
 
         val vm: AuthViewModel = getViewModel(factory)
         vm.state.observe(this, Observer { authStateChanged(it) })
         vm.event.observe(this, Observer {
-            Snackbar.make(parentCL, it, Snackbar.LENGTH_LONG).show()
+            Snackbar.make(binding.parentCL, it, Snackbar.LENGTH_LONG).show()
         })
     }
 
