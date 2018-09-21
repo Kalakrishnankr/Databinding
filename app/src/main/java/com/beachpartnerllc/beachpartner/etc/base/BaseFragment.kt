67e29ed161a0f4dec623f5479607abb68d3cdc1d
Abraham Mathew <abraham.mathew@seqato.com>
2018-09-21 13:04:03 +0530

    Profile : d3bbc41

--- app/src/main/java/com/beachpartnerllc/beachpartner/etc/base/BaseFragment.kt
+++ app/src/main/java/com/beachpartnerllc/beachpartner/etc/base/BaseFragment.kt
@@ -8,4 +8,9 @@ import com.beachpartnerllc.beachpartner.etc.di.Injectable
  * @author Samuel Robert <sam@spotsoon.com>
  * @created on 31 May 2018 at 1:50 PM
  */
-abstract class BaseFragment : Fragment(), Injectable
+abstract class BaseFragment : Fragment(), Injectable {
+	val PICK_IMAGE_REQUEST = 0
+	val PICK_VIDEO_REQUEST = 1
+	val CAMERA_PERMISSION  = 21
+	val VIDEO_PERMISSION   = 22
+}
