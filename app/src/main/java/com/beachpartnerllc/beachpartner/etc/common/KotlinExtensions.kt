587ebe21a2fd4df4fd798fd90058904e5cf526f0
Samuel Robert <samuel.robert@seqato.com>
2018-09-28 13:03:34 +0530

    connection: Zip function on live data causing runtime exception
    Signed-off-by: Samuel Robert <samuel.robert@seqato.com>

--- app/src/main/java/com/beachpartnerllc/beachpartner/etc/common/KotlinExtensions.kt
+++ app/src/main/java/com/beachpartnerllc/beachpartner/etc/common/KotlinExtensions.kt
@@ -25,46 +25,51 @@ import java.util.*
 
 /********         Arch Components         ***************/
 inline fun <reified T : ViewModel> Fragment.getViewModel(factory: ViewModelProvider.Factory, ofActivity: Boolean = true): T {
-    return if (ofActivity) ViewModelProviders.of(activity!!, factory)[T::class.java]
-    else ViewModelProviders.of(this, factory)[T::class.java]
+	return if (ofActivity) ViewModelProviders.of(activity!!, factory)[T::class.java]
+	else ViewModelProviders.of(this, factory)[T::class.java]
 }
 
 inline fun <reified T : ViewModel> AppCompatActivity.getViewModel(factory: ViewModelProvider.Factory): T {
-    return ViewModelProviders.of(this, factory)[T::class.java]
+	return ViewModelProviders.of(this, factory)[T::class.java]
 }
 
+fun <T> zip(vararg observables: LiveData<T>): MediatorLiveData<T> {
+	val mediator = MediatorLiveData<T>()
+	observables.forEach { mediator.addSource(it) { value -> mediator.value = value } }
+	return mediator
+}
 
 /********          View Components          **************/
 inline fun <reified T : RecyclerView.ViewHolder> ViewGroup.create(createHolder: (View) -> T, @LayoutRes res: Int): T {
-    val inflater = LayoutInflater.from(context)
-    val view = inflater.inflate(res, this, false)
-    return createHolder(view)
+	val inflater = LayoutInflater.from(context)
+	val view = inflater.inflate(res, this, false)
+	return createHolder(view)
 }
 
+
 inline fun <T : RecyclerView.ViewHolder, R : ViewDataBinding> ViewGroup.bind(construct: (R) -> T, @LayoutRes resId: Int): T {
-    val inflater = LayoutInflater.from(context)
-    val binding: R = DataBindingUtil.inflate(inflater, resId, this, false)
-    return construct(binding)
+	val inflater = LayoutInflater.from(context)
+	val binding: R = DataBindingUtil.inflate(inflater, resId, this, false)
+	return construct(binding)
 }
 
 inline fun <reified R : ViewDataBinding> ViewGroup.bind(@LayoutRes resId: Int): R {
-    val inflater = LayoutInflater.from(context)
-    return DataBindingUtil.inflate(inflater, resId, this, false)
+	val inflater = LayoutInflater.from(context)
+	return DataBindingUtil.inflate(inflater, resId, this, false)
 }
 
 inline fun <reified R : ViewDataBinding> LayoutInflater.bind(@LayoutRes layoutResId: Int, parent: ViewGroup?): R {
-    return DataBindingUtil.inflate(this, layoutResId, parent, false)
+	return DataBindingUtil.inflate(this, layoutResId, parent, false)
 }
 
 inline fun <reified T : AppCompatActivity> Activity.startActivity(
-        data: Bundle = Bundle(),
-        isForResult: Boolean = false,
-        requestCode: Int = 0
-) {
-    val intent = Intent(this, T::class.java)
-    intent.putExtras(data)
-    if (isForResult) startActivityForResult(intent, requestCode)
-    else startActivity(intent)
+	data: Bundle = Bundle(),
+	isForResult: Boolean = false,
+	requestCode: Int = 0) {
+	val intent = Intent(this, T::class.java)
+	intent.putExtras(data)
+	if (isForResult) startActivityForResult(intent, requestCode)
+	else startActivity(intent)
 }
 
 /*******              String                 ***************/
@@ -73,17 +78,11 @@ fun String?.isEmail() = this != null && Patterns.EMAIL_ADDRESS.matcher(this).mat
 
 fun String?.isMobile() = this != null && Patterns.PHONE.matcher(this).matches()
 
-fun String?.isPassword() = this != null && length > 7
+fun String?.isPassword() = this != null && length > 7 && this.trim().length == length
 
-fun String?.isName() = this != null && this.matches("[a-zA-Z]+".toRegex())
+fun String?.isName() = this != null && this.trim().matches("[a-z A-Z]+".toRegex())
 
 fun Date.truncateTime(): Date {
-    val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-    return formatter.parse(formatter.format(this))
-}
-
-inline fun <reified T> zip(vararg observables: LiveData<T>): MediatorLiveData<T> {
-    val mediator = MediatorLiveData<T>()
-    observables.forEach { mediator.addSource(it) { value -> mediator.value = value } }
-    return mediator
+	val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+	return formatter.parse(formatter.format(this))
 }
